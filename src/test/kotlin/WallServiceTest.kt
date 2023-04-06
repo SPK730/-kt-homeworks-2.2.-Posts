d94d8c0aa4f8ec2c4ba1c12796982e328b9ea6d3
twin730 <twin730@mail.ru>
2023-04-06 21:52:46 +0300

    Commit with all tests add added

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -5,7 +5,7 @@ import org.junit.Assert.*
 
 class WallServiceTest {
     @Test
-    fun add() {
+    fun addPostTest() {
         val service = WallService
         //service.add(Post(1, Likes(0)))
         val result = service.add(
@@ -14,19 +14,28 @@ class WallServiceTest {
         assertTrue(result.id > 0)
     }
 
+    @Test
+    fun updateExistingPostTest() {// создаём целевой сервис
+        val service = WallService  // заполняем несколькими постами
+        service.add(Post(1, Likes(0), 1547, 198460000, "какой-то текст", "(c)", "copy", true, false, false))
+        service.add(Post(2, Likes(4), 1414, 198760000, "другой текст", "(c)", "copy", true, false, false))
+        service.add(Post(3, Likes(10), 1247, 199460000, "еще какой-то текст", "(c)", "copy", true, false, false))
+        // создаём информацию об обновлении
+        val update = Post(1, Likes(10), 1147, 190460000, "и еще какой-то текст", "(c)", "copy", true, false, false)
+        val result = service.update(update)
+        assertFalse(result) // проверяем результат (используйте assertTrue или assertFalse)
+    }
 
-//    fun updateExisting() {// создаём целевой сервис
-//        val service = WallService()  // заполняем несколькими постами
-//        service.add(Post(/* заполняете поля */))
-//        service.add(Post(/* заполняете поля */))
-//        service.add(Post(/* заполняете поля */))
-//        // создаём информацию об обновлении
-//        val update = Post(/* заполняете поля */)
-//
-//        // выполняем целевое действие
-//        val result = service.update(update)
-//
-//        // проверяем результат (используйте assertTrue или assertFalse)
-//        assertTrue(result)
-//    }
+    @Test
+    fun updateNotExistingPostTest() {// создаём целевой сервис
+        val service = WallService  // заполняем несколькими постами
+        service.add(Post(1, Likes(0), 1547, 198460000, "какой-то текст", "(c)", "copy", true, false, false))
+        service.add(Post(2, Likes(4), 1414, 198760000, "другой текст", "(c)", "copy", true, false, false))
+        service.add(Post(3, Likes(10), 1247, 199460000, "еще какой-то текст", "(c)", "copy", true, false, false))
+        // создаём информацию об обновлении
+        val update = Post(4, Likes(10), 1147, 190460000, "и еще какой-то текст", "(c)", "copy", true, false, false)
+        val result = service.update(update)
+        assertTrue(result) // проверяем результат (используйте assertTrue или assertFalse)
+    }
+
 }
